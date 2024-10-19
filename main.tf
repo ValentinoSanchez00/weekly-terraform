55b9fa85a5ad51302ac8ab812a9ed86cff974afe
ValentinoSanchez00 <valentinosanchezraverta@gmail.com>
2024-10-19 14:52:56 +0200

    add first configuration

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.5.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  use_cli         = true
+}
+
+
+module "vnet" {
+  source = "github.com/stemdo-labs/terraform-weekly-exercise-ValentinoSanchez00/modulo-weekly-exercise/vnet"
+  gr_name = var.gr_name
+  vnet_address_space = var.address_space
+  location=var.location
+}
+
+module "subnet" {
+  source = "github.com/stemdo-labs/terraform-weekly-exercise-ValentinoSanchez00/modulo-weekly-exercise/subnet"
+  gr_name = var.gr_name
+  vnet_name = module.vnet.vnet_name
+  vm_definitions = var.vm_definitions
+}
+module "networkInterface" {
+  source = "github.com/stemdo-labs/terraform-weekly-exercise-ValentinoSanchez00/modulo-weekly-exercise/azure_network_interface"
+  gr_name = var.gr_name
+  vm_definitions = var.vm_definitions
+  location=var.location
+  subnet_id = module.subnet.subnet_id
+}
+
+module "virtualMachine" {
+  source = "github.com/stemdo-labs/terraform-weekly-exercise-ValentinoSanchez00/modulo-weekly-exercise/virtual_machine"
+  vm_definitions = var.vm_definitions
+  location=var.location
+  rg_name=var.gr_name
+  network_interface_ids = module.networkInterface.network_interface_ids
+
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "tenant_id" {
+  description = "Tenant ID"
+  type = string
+  default = "2835cee8-01b5-4561-b27c-2027631bcfe1"
+}
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  type = string
+  default = "86f76907-b9d5-46fa-a39d-aff8432a1868"
+}
